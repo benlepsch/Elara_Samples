6853dad3a86bde3c26887b006838620fb226f9d4
Ben Lepsch <blepsch@gmail.com>
2021-07-22 15:02:30 -0400

    remember to fix constantread as well

--- app/src/main/java/Samples/ConstantRead.java
+++ app/src/main/java/Samples/ConstantRead.java
@@ -11,7 +11,6 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import com.thingmagic.ECTConstants;
 import com.thingmagic.ElaraJSONParser;
-import com.thingmagic.ElaraTransportListener;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -42,30 +41,29 @@ public class ConstantRead {
             }
 
             StopRead msg = new StopRead();
-            ElaraTransportListener elaraTransportListener = null;
             ElaraJSONParser ejsonp = new ElaraJSONParser();
 
             String stop = ejsonp.formJSONCommand(ECTConstants.STOP_RZ);
             System.out.println("Command: " + stop);
-            String response = (String) msg.sendMessage(reader, stop, elaraTransportListener);
+            String response = (String) msg.sendMessage(reader, stop);
             System.out.println("Response: " + response); 
 
             // Get reader info
             String info = ejsonp.formJSONCommand(ECTConstants.GET_INFO_FIELDS_ALL);
             System.out.println("Command: " +info);
-            String rec = (String) msg.sendMessage(reader, info, elaraTransportListener);
+            String rec = (String) msg.sendMessage(reader, info);
             System.out.println("Response: " +rec);
            
             // Set WorkFlow
             String Workflow = ejsonp.formJSONCommand(ECTConstants.SET_CFG_MODE_MONITOR);
             System.out.println("Command: " + Workflow);
-            String response1 = (String) msg.sendMessage(reader, Workflow, elaraTransportListener);
+            String response1 = (String) msg.sendMessage(reader, Workflow);
             System.out.println("Response: " + response1);
             
             // Start BulkTagRead
             String Startread = ejsonp.formJSONCommand(ECTConstants.START_RZ);
             System.out.println("Command: " + Startread);
-            String response2 = (String) msg.sendMessage(reader, Startread, elaraTransportListener);
+            String response2 = (String) msg.sendMessage(reader, Startread);
             System.out.println("Response: " + response2);
             // Thread.sleep(5000);
             // String data=(String) msg.receiveMessage(reader.getInputStream(), elaraTransportListener);
@@ -75,7 +73,7 @@ public class ConstantRead {
             int count = 0;
             do {
                 Thread.sleep(1000);
-                String data = (String) msg.receiveMessage(reader.getInputStream(), elaraTransportListener);
+                String data = (String) msg.receiveMessage(reader.getInputStream());
                 if (!data.equals(""))
                     System.out.println(data);
                 count ++;
@@ -84,7 +82,7 @@ public class ConstantRead {
             // Stop reading
             String Stopread = ejsonp.formJSONCommand(ECTConstants.STOP_RZ);
             System.out.println("Command: " + Stopread);
-            String response3 = (String) msg.sendMessage(reader, Stopread, elaraTransportListener);
+            String response3 = (String) msg.sendMessage(reader, Stopread);
             System.out.println("Response: " + response3);
 
             reader.closePort();
